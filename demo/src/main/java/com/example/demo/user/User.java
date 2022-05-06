9563bec59c5d3fa59127ca2d064fff3637e086d1
Serena <siriuswusss@gmail.com>
2022-05-06 17:08:31 +0800

    Create User Class
    Include three constructors, getters, setters and toString methods.

--- demo/src/main/java/com/example/demo/user/User.java
+++ demo/src/main/java/com/example/demo/user/User.java
@@ -1,5 +1,104 @@
 package com.example.demo.user;
 
+import java.time.LocalDateTime;
+
 public class User {
-    
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+    private String bio;
+    private LocalDateTime dateOfRegistration;
+
+    public User() {
+    }
+
+    // Constructor with all parameters
+    public User(Long userId,
+                String username,
+                String email,
+                String password,
+                String bio,
+                LocalDateTime dateOfRegistration) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.bio = bio;
+        this.dateOfRegistration = dateOfRegistration;
+    }
+
+    // Constructor without the userId
+    public User(String username,
+                String email,
+                String password,
+                String bio,
+                LocalDateTime dateOfRegistration) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.bio = bio;
+        this.dateOfRegistration = dateOfRegistration;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public LocalDateTime getDateOfRegistration() {
+        return dateOfRegistration;
+    }
+
+    public void setDateOfRegistration(LocalDateTime dateOfRegistration) {
+        this.dateOfRegistration = dateOfRegistration;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", bio='" + bio + '\'' +
+                ", dateOfRegistration=" + dateOfRegistration +
+                '}';
+    }
 }
+
