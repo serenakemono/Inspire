531a07aa63eaa8783c59cd456a83bf06cfc314e9
Serena <siriuswusss@gmail.com>
2022-05-07 15:37:48 +0800

    Add DeleteMapping

--- demo/src/main/java/com/example/demo/user/UserController.java
+++ demo/src/main/java/com/example/demo/user/UserController.java
@@ -1,7 +1,9 @@
 package com.example.demo.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,10 @@ public class UserController {
     @PostMapping
     public void registerNewUser(@RequestBody AppUser user) {
         userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
     }
 }
